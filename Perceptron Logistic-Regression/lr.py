79b64a458d5f48be3e151dc90f0a690464baf49f
Kaiyu Song <kaiyusong1994@gmail.com>
2018-10-02 13:56:40 -0700

    code

---
+++ Perceptron Logistic-Regression/lr.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+#
+# CIS 472/572 - Logistic Regression Template Code
+#
+# Author: Daniel Lowd <lowd@cs.uoregon.edu>
+# Date:   2/9/2018
+#
+# Please use this code as the template for your solution.
+#
+import sys
+import re
+from math import log
+from math import exp
+from math import sqrt
+
+MAX_ITERS = 100
+
+# Load data from a file
+def read_data(filename):
+  f = open(filename, 'r')
+  p = re.compile(',')
+  data = []
+  header = f.readline().strip()
+  varnames = p.split(header)
+  namehash = {}
+  for l in f:
+    example = [int(x) for x in p.split(l.strip())]
+    x = example[0:-1]
+    y = example[-1]
+    data.append( (x,y) )
+  return (data, varnames)
+
+
+# Train a logistic regression model using batch gradient descent
+def train_lr(data, eta, lam):
+  numvars = len(data[0][0])
+  w = [0.0] * numvars
+  b = 0.0
+  for ite in range(MAX_ITERS):
+    gradient_w = [0.0] * numvars
+    gradient_b = 0.0
+    for (x,y) in data:
+      gradient = -(1/(1 + exp(y * (dot(w, x) + b))))*y
+      for j in range(0,numvars):
+        gradient_w[j] += x[j] * gradient
+      gradient_b += gradient
+    if (sqrt(dot(gradient_w, gradient_w) + gradient_b * gradient_b) < eta):
+      return (w,b)
+    for k in range(numvars):
+      w[k] -= eta * (gradient_w[k] + (lam * w[k]))
+    b -= eta * gradient_b
+  return (w,b)
+
+# Predict the probability of the positive label (y=+1) given the
+# attributes, x.
+def predict_lr(model, x):
+  (w,b) = model
+  return 1/(1+exp(-(dot(x,w)+b)))
+
+def dot(l1, l2):
+  result = 0
+  for i in range(len(l1)):
+    result += l1[i] * l2[i]
+  return result
+
+# Load train and test data.  Learn model.  Report accuracy.
+def main(argv):
+  if (len(argv) != 5):
+    print('Usage: lr.py <train> <test> <eta> <lambda> <model>')
+    sys.exit(2)
+  (train, varnames) = read_data(argv[0])
+  (test, testvarnames) = read_data(argv[1])
+  eta = float(argv[2])
+  lam = float(argv[3])
+  modelfile = argv[4]
+
+  # Train model
+  (w,b) = train_lr(train, eta, lam)
+
+  # Write model file
+  f = open(modelfile, "w+")
+  f.write('%f\n' % b)
+  for i in range(len(w)):
+    f.write('%s %f\n' % (varnames[i], w[i]))
+
+  # Make predictions, compute accuracy
+  correct = 0
+  for (x,y) in test:
+    prob = predict_lr( (w,b), x )
+    print(prob)
+    if (prob - 0.5) * y > 0:
+      correct += 1
+  acc = float(correct)/len(test)
+  print("Accuracy: ",acc)
+
+if __name__ == "__main__":
+  main(sys.argv[1:])
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Machine-Learning-Algorithums-Perceptron-Logistic-Regression
+type: 
+python3 perceptron.py <train> <test> <model>
+python3 lr.py <train> <test> <eta> <lambda> <model>
+to get accuracy.
